5d3faf5f2488160ae39db1c394fc40911684cf38
C <chrismatusemail@gmail.com>
2020-03-10 00:59:38 +0000

    Finger animation basically working.

--- src/App.js
+++ src/App.js
@@ -104,17 +104,22 @@ class PlayerNames extends React.Component {
   render() {
     return Object.keys(this.state.playerNamesDisplay).map(player => {
       return (
-        // <div className="playerNameTextWrapper">
         <div id={`${player}`} className={`${player}Details`}>
+          <Finger
+            player={player}
+            isItPlayerOneTurn={this.props.isItPlayerOneTurn}
+            handleTransitionToggle={this.props.handleTransitionToggle}
+            transitionToggle={this.props.transitionToggle}
+          />
           <h2 className="playerNameText">
-            {this.props.isItPlayerOneTurn && player === "p1" ? "☝" : ""}
-            {!this.props.isItPlayerOneTurn && player === "p2" ? "☝" : ""}
-            <br />
             {this.state.playerNamesDisplay[player]}
           </h2>
-          <form onSubmit={this.handlePlayerSubmit.bind(this, player)}>
+          <form
+            className={this.props.hasTournamentBegun ? "invisible" : ""}
+            onSubmit={this.handlePlayerSubmit.bind(this, player)}
+          >
             <input
-              onChange={this.handlePlayerNameChange.bind(this, player)} //This is how you pass arguments to an event handler.
+              onChange={this.handlePlayerNameChange.bind(this, player)}
               id={`${player}name`}
               name={`${player}name`}
               type="text"
@@ -128,10 +133,69 @@ class PlayerNames extends React.Component {
             </button>
           </form>
         </div>
-        // </div>
       );
     });
   }
+}
+
+class Finger extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="fingerWrapper">
+        {this.props.transitionToggle ? (
+          <h5
+            className={
+              this.props.player === "p1"
+                ? "normalAnimation"
+                : "reverseAnimation"
+            }
+          >
+            <i className="theFinger">
+              {this.props.isItPlayerOneTurn && this.props.player === "p1"
+                ? "☝🏾"
+                : ""}
+              {!this.props.isItPlayerOneTurn && this.props.player === "p2"
+                ? "☝🏾"
+                : ""}
+              {this.props.handleTransitionToggle(false, 1000)}
+              {/* {setTimeout(() => {
+                this.props.handleTransitionToggle(true);
+              }, 500)} */}
+            </i>
+          </h5>
+        ) : (
+          <div className="theFinger">
+            {this.props.isItPlayerOneTurn && this.props.player === "p1"
+              ? "☝🏾"
+              : ""}
+            {!this.props.isItPlayerOneTurn && this.props.player === "p2"
+              ? "☝🏾"
+              : ""}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+function ExtraButtons(props) {
+  return (
+    <div
+      className="middleBottom"
+      className={props.hasTournamentBegun ? "invisible" : ""}
+    >
+      <button
+        className="startTourneyButton"
+        onClick={() => {
+          props.tournBegunHandler();
+        }}
+      >
+        Start Tournament!
+      </button>
+    </div>
+  );
 }
 
 class Board extends React.Component {
@@ -185,6 +249,7 @@ class Board extends React.Component {
         console.log(newBoard);
         newBoard[row][column] = this.props.isItPlayerOneTurn ? "X" : "O";
         this.props.turnHandler();
+        this.props.handleTransitionToggle(true);
         return {
           board: newBoard
         };
@@ -196,37 +261,34 @@ class Board extends React.Component {
 
   render() {
     return (
-      // <div className="over-grid">
       <div className="wrapper">
         {this.state.board.map((row, indexRow) =>
           row.map((column, indexColumn) => {
             return (
               <div
                 id={`box ${indexRow}-${indexColumn}`}
-                // ref={this.clickedSquare}
                 onClick={() => {
                   this.handleClick(this, indexRow, indexColumn);
                 }}
                 className={`squares`}
-                //className={`squares ${this.state.background}`}
               >
                 <div className="squareSymbolWrapper">
                   {this.state.board[indexRow][indexColumn]}
                 </div>
               </div>
             );
-            //return <div id="box a" class="squares"></div>;
           })
         )}
       </div>
-      // </div>
     );
   }
 }
 
 class App extends React.Component {
   state = {
-    isItPlayerOneTurn: true
+    isItPlayerOneTurn: true,
+    hasTournamentBegun: false,
+    transitionToggle: false
   };
 
   turnHandler = () => {
@@ -235,7 +297,31 @@ class App extends React.Component {
     });
   };
 
+  handleTransitionToggle = (boolean, timeout) => {
+    console.log(boolean, timeout);
+    console.log("in handletransitiontoggle");
+    if (timeout === undefined) {
+      this.setState({
+        transitionToggle: boolean
+      });
+    } else {
+      console.log("ready to timeout");
+      setTimeout(() => {
+        this.setState({
+          transitionToggle: boolean
+        });
+      }, timeout);
+    }
+  };
+
+  tournBegunHandler = () => {
+    this.setState({
+      hasTournamentBegun: !this.state.hasTournamentBegun
+    });
+  };
+
   render() {
+    console.log("re-rendering");
     return (
       <div className="App">
         <header className="Ticcy Taccy Toe"></header>
@@ -244,8 +330,18 @@ class App extends React.Component {
           <Board
             isItPlayerOneTurn={this.state.isItPlayerOneTurn}
             turnHandler={this.turnHandler}
+            handleTransitionToggle={this.handleTransitionToggle}
           />
-          <PlayerNames isItPlayerOneTurn={this.state.isItPlayerOneTurn} />
+          <PlayerNames
+            isItPlayerOneTurn={this.state.isItPlayerOneTurn}
+            hasTournamentBegun={this.state.hasTournamentBegun}
+            transitionToggle={this.state.transitionToggle}
+            handleTransitionToggle={this.handleTransitionToggle}
+          />
+          <ExtraButtons
+            tournBegunHandler={this.tournBegunHandler}
+            hasTournamentBegun={this.state.hasTournamentBegun}
+          />
         </div>
         {/* </div> */}
         {/* <div className="container">
